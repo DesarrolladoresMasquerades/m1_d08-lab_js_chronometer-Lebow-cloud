ce1b19f38b25403efbc6f6db34fd574fd3a8a1de
Daniel Milan <danielmilanvera@gmail.com>
2022-01-26 20:26:18 +0100

    Not running time

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,49 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+  this.intervalId = setInterval(()=>{
+    this.currentTime++
+    callback()
+  }, 1 * 1000)
+
   }
 
   getMinutes() {
-    // ... your code goes here
+    
+    let minutes = this.currentTime/60 
+    return Math.floor(minutes)
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = this.currentTime % 60
+    return seconds
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let bikini = value.toString()
+
+    if (bikini.length < 2) return ("0" + bikini) 
+    else return bikini
+  
   }
 
   stop() {
-    // ... your code goes here
+
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+   return this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds())
+
+   
   }
 }
 
